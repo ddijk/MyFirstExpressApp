d909b20f36f451229b11a01afee01539cee80b50
ddijk <dick@dijkrosoft.nl>
2019-03-16 20:00:55 +0100

    Add Post request

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const bodyparser = require('body-parser');
 
 const app = express();
 
-PORT=8888;
+PORT= process.env.PORT || 8888;
 
 app.get("/status", ( req, res) => {
           var localtime = (new Date()).toLocaleTimeString();
           res.status(200).send(`Server send ${localtime}`);
 });
+
+app.use(bodyparser.json());
+app.post("/login", (req, res) => {
+
+     const username = req.body.username;
+     res.send(`You logged in with ${username}.`);
+});
+   
 
 app.get("*", (req, res) => {
 	res.sendStatus(404);
